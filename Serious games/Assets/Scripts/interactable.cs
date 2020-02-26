b590619291d98c99466fab29eba12de3c96c8a25
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-02-26 16:21:14 +0100

    Interactable objects, but has an issue

--- Serious games/Assets/Scripts/interactable.cs
+++ Serious games/Assets/Scripts/interactable.cs
@@ -3,9 +3,39 @@
 public class interactable : MonoBehaviour
 {
     public float radius = 3f;
+    Transform player;
+    bool isFocus = false;
 
-    void OnDrawGizmosSelected ()
+    public virtual void Interact()
     {
-        Gizmos
+        //here is the actual interaction, just call the method
+        Debug.Log("interact");
     }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public Update ()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+        if (distance <= radius) Interact();
+    }
+
+
+    /* FOCUS MECHANIC
+     * 
+    public void Focused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+
+    }
+    public void Defocused ()
+    {
+        isFocus = false;
+        player = null;
+    }*/
 }
