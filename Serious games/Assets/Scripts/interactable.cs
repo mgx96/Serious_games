be7f54c9c171f239ec70dbc58c156401892158e6
Alex  Kokkinidis <alexandros.kokkinidis.0817@student.uu.se>
2020-03-04 14:20:25 +0100

    sorting shit out
    sorting shit out

--- Serious games/Assets/Scripts/interactable.cs
+++ Serious games/Assets/Scripts/interactable.cs
@@ -3,9 +3,55 @@
 public class interactable : MonoBehaviour
 {
     public float radius = 3f;
+    public Transform player;
+    bool interacting = false;
+    // bool isFocus = false;
 
-    void OnDrawGizmosSelected ()
+    public virtual void Interact()
     {
-        
+
+        //here is the actual interaction, just call the method
+        Debug.Log("interact");
+
+
     }
+
+    #region VisualFeedback
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+    #endregion VisualFeedback
+
+    public void Update()
+    {
+
+        float distance = Vector3.Distance(player.position, transform.position);
+        if ((distance <= radius) && (interacting == false))
+        {
+            Interact();
+            interacting = true;
+        }
+        if (distance > radius) 
+        {
+            interacting = false; 
+        }
+
+    }
+
+
+    /* FOCUS MECHANIC
+     * 
+    public void Focused (Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+
+    }
+    public void Defocused ()
+    {
+        isFocus = false;
+        player = null;
+    }*/
 }
